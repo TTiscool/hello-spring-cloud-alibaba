f5c84fc3bda834b3773afcd612e45994f5f802c4
TTiscool <357524663@qq.com>
2020-06-07 14:11:28 +0800

    test

---
+++ hello-spring-cloud-alibaba/src/test/java/com/toplist/hello/spring/cloud/alibaba/HelloSpringCloudAlibabaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toplist.hello.spring.cloud.alibaba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringCloudAlibabaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
